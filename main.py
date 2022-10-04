6ccb85c491f56381fab8e7c04e9280c9b066195e
lcl <2328638898@qq.com>
2022-10-04 16:38:20 +0800

    hi-pycharm

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+
+
+def print_hi(name):
+
+    print(f'Hi, everyone')  
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
